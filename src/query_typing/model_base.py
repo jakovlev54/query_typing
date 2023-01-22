809e46ebe1f539e97c46071d0c71598d554bbc11
Alexandr Yakovlev <jovi2002@mail.ru>
2023-01-23 02:45:48 +0700

    fix bags

--- src/query_typing/model_base.py
+++ src/query_typing/model_base.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import List, TypeVar, Generic, Any
 
 
 @dataclass
@@ -6,17 +7,36 @@ class ModelBase:
     id: int
 
 
-class QueryBase:
+T = TypeVar('T', bound=ModelBase)
+
+
+class NotFoundError(Exception):
+    pass
+
+
+class NotFoundErrorMeta(type):
+
+    def __init__(cls: Any, name: Any, bases: Any, attrs: Any):
+        super().__init__(name, bases, attrs)
+        cls.NotFoundError = type("NotFoundError",
+                                 (NotFoundError,),
+                                 {
+                                     "__qualname__": cls.__qualname__ + ".NotFoundError"
+                                 })
+
+
+class QueryBase(Generic[T], metaclass=NotFoundErrorMeta):
+
     class NotFoundError(Exception):
         pass
 
-    def __init__(self, models):
+    def __init__(self, models: List[T]):
         self.models = models
 
-    def all(self):
+    def all(self) -> List[T]:
         return self.models
 
-    def get(self, id: int):
+    def get(self, id: int) -> T:
         for model in self.models:
             if model.id == id:
                 return model
